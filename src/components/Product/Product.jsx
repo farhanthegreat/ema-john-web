56c90daff84f2c8a824aca084859adb32dd3e587
farhanthegreat <farhantuber@gmail.com>
2023-08-13 12:10:56 +0600

    btn hover added

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -2,7 +2,12 @@ import React from 'react';
 import './Product.css';
 
 const Product = (props) => {
+    console.log(props);
     const {img, name, seller, ratings, price} = props.product;
+    
+    const handleAddToCart = props.handleAddToCart;
+     
+
     return (
         <div className='product'>
            <img src={img} alt="" /> 
@@ -12,7 +17,7 @@ const Product = (props) => {
             <p>Manufacturer: {seller}</p>
             <p>Rating: {ratings} Star</p>
            </div>
-           <button className='btn-cart'>Add To Cart</button>
+           {/* <button onClick={() => handleAddToCart(props.product)} className='btn-cart'>Add To Cart</button> */}
         </div>
 
     );
